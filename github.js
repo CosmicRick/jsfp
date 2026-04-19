430ed5164463d7310022e578ed8ab28d3ff5dc09
CosmicRick <supradiproy737@gmail.com>
2026-04-19 13:47:24 +0530

    commit and push

--- github.js
+++ github.js
@@ -1,17 +1,15 @@
 document.querySelector('#Check').addEventListener('click', (e)=>{
     e.preventDefault()
     let users = (document.querySelector('#SerachBox')).value
-    console.log(users)
 const baseurl = `https://api.github.com/users/${users}`
 const req = new XMLHttpRequest();
 req.open('GET', baseurl)
 req.onreadystatechange = function(){
     if(req.readyState === 4){
     let  data = JSON.parse(this.responseText)
-    console.log(data)
     document.querySelector('.usershowcase').innerHTML = 
     `
-    <img src = ${data.avatar_url} style="width: 14rem;">
+    <img src = ${data.avatar_url}>
     <h3>${data.name}</h3>
     `
     }
